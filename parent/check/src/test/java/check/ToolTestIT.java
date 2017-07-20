215374661dfeadb3e1716e63e383dabc9b8190ab
Jakub Siberski <jakub.siberski@numberfour.eu>
2017-07-20 16:33:34 +0200

    make example eclipse friendly

--- parent/check/src/test/java/check/ToolTestIT.java
+++ parent/check/src/test/java/check/ToolTestIT.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.eclipse.core.runtime.Platform;
+import org.junit.Before;
 import org.junit.Test;
 
 import check.utils.ProcessUtil;
@@ -15,6 +17,11 @@ import check.utils.SizeUtil;
 
 /**
  * Tests for a given command line tool.
+ * 
+ * This is integration test that depends on artifacts build in maven life cycle
+ * not covered by eclipse (not even with m2e). The test is skipped when in
+ * eclipse environment, but it is executed when build is executed on command
+ * line (e.g. in the CI).
  */
 public class ToolTestIT {
 
@@ -24,6 +31,13 @@ public class ToolTestIT {
 	private static final String JAVA_PATH = System.getProperty("java.home") + "/bin/java";
 	private static final int OK = 0;
 	private static final long ONE_MB = 1024 * 1024;
+
+	@Before
+	public void beforeMethod() {
+		//poor man's check if we are running from maven command line
+		//see https://stackoverflow.com/a/24045651
+		org.junit.Assume.assumeNotNull("Skip maven based test.", System.getenv("MAVEN_CMD_LINE_ARGS"));
+	}
 
 	/**
 	 * Check if cli tool is accessible and callable.
@@ -46,5 +60,4 @@ public class ToolTestIT {
 		assertEquals("process calling tool returned error code " + exitCode, OK, exitCode);
 
 	}
-
 }
